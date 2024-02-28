84cfb567b4323deecb873332b60af519053f26e3
Marcus Oliveira <maarcus.soares@gmail.com>
2024-02-28 11:46:06 -0300

    adicionando funsão no js

--- Main.js
+++ Main.js
@@ -1,1 +1,18 @@
-var selecionarCampus = document.getElementById('selecionar_campus');
+//var selecionarCampus = document.getElementById('selecionar_campus');
+
+const selecionarCampus = document.querySelector('.selecionar_campus');
+const vassouras = document.querySelector('.vassouras');
+const marica = document.querySelector('.marica');
+const saquarema = document.querySelector('.saquarema');
+
+selecionarCampus.addEventListener('change', (event) => {
+   console.log(event.target.value);
+   if (event.target.value === 'vassouras') {
+        window.location.href = '../Pages/Campus_vassouras.html';
+   } else if (event.target.value === 'marica') {
+        window.location.href = '../Pages/Campus_marica.html';
+    } else if (event.target.value === 'saquarema') {
+        window.location.href = '../Pages/Campus_saquarema.html';
+   }
+})
+
