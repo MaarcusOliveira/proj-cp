542b4d5a1cee30db905da5cc94c6e17d40264124
Marcus Oliveira <maarcus.soares@gmail.com>
2024-02-28 17:26:48 -0300

    atualizando o main.js, corrigindo o html

--- Main.js
+++ Main.js
@@ -1,18 +1,20 @@
-//var selecionarCampus = document.getElementById('selecionar_campus');
+ //var selecionarCampus = document.getElementById('selecionar_campus');
 
 const selecionarCampus = document.querySelector('.selecionar_campus');
-const vassouras = document.querySelector('.vassouras');
-const marica = document.querySelector('.marica');
-const saquarema = document.querySelector('.saquarema');
+const fundoBordo = document.querySelector('.fundo_Bordo');
 
 selecionarCampus.addEventListener('change', (event) => {
    console.log(event.target.value);
    if (event.target.value === 'vassouras') {
-        window.location.href = '../Pages/Campus_vassouras.html';
+       // window.location.href = '../Pages/Campus_vassouras.html';
+        fundoBordo.classList.add('fundo_bordo--desabilitado');
+        console.log('vassouras');
    } else if (event.target.value === 'marica') {
         window.location.href = '../Pages/Campus_marica.html';
     } else if (event.target.value === 'saquarema') {
         window.location.href = '../Pages/Campus_saquarema.html';
+    
    }
 })
 
+ 
